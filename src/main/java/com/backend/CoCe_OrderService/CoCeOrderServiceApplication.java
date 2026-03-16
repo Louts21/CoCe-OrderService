f486fd1631edf459cd2fbbd24c7206600ab3ae7d
Louts21 <72473132+Louts21@users.noreply.github.com>
2026-03-16 16:54:49 +0100

    initialized order service of project

---
+++ src/main/java/com/backend/CoCe_OrderService/CoCeOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.CoCe_OrderService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCeOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCeOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/CoCe_OrderService/CoCeOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.CoCe_OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCeOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
