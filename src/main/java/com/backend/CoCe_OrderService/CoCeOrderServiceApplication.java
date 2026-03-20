8535fc5860b16a3f367062932472690d2a1854c9
Louts21 <72473132+Louts21@users.noreply.github.com>
2026-03-20 13:49:28 +0100

    added additional cors for order

--- src/main/java/com/backend/CoCe_OrderService/CoCeOrderServiceApplication.java
+++ src/main/java/com/backend/CoCe_OrderService/CoCeOrderServiceApplication.java
@@ -20,6 +20,7 @@ public class CoCeOrderServiceApplication {
 			@Override
 			public void addCorsMappings(@NonNull CorsRegistry registry) {
 				registry.addMapping("/api/order").allowedOrigins("http://localhost:5173");
+				registry.addMapping("/api/order/**").allowedOrigins("http://localhost:5173");
 			}
 		};
 	}
